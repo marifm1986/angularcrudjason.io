0bccbfa0b7b913369e51097b1cdf48dd7295ebef
arif matubbber <43994458+marifm1986@users.noreply.github.com>
2020-09-17 02:08:00 +0600

    angular crud

---
+++ contact.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+
+  constructor(private _http: HttpClient) { }
+
+
+  createUser(user) {
+    return this._http.post("http://localhost:3000/users", user);
+  }
+
+  getAllUser() {
+    return this._http.get("http://localhost:3000/users");
+  }
+  updateUser(user) {
+    return this._http.put("http://localhost:3000/users/" + user.id, user);
+  }
+  deleteUser(user) {
+    return this._http.delete("http://localhost:3000/users/" + user.id);
+  }
+
+
+
+}
